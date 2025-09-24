7a898ace6bbed22db8fc45b4c4b3730cc1a0f83d
Cory Echols <cechols@laddernow.com>
2025-09-24 09:05:29 -0400

    Initial commit

---
+++ example/example/pages/1_kpop.py
@@ -0,0 +1,4 @@
+import streamlit as st;
+st.title("I like Kpop")
+st.text("G-idle is the best of all!")
+st.image("images/gidle.jpg")
---
+++ example/example/pages/2_birds.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("I like birds")
+st.text("Cardinals are superior to all others.  This one is called Fred")
+st.image("images/cardinal.jpg");
